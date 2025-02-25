bbb2db49ef145611eba3fd4599e079e06dcdb54c
tommasoc80 <t.caselli@gmail.com>
2025-02-25 16:49:59 +0100

    results

---
+++ output/qwen25-3B/MD_Agreement/eval_script.py
@@ -0,0 +1,30 @@
+import sys
+import pandas
+import pandas as pd
+from sklearn.metrics import classification_report
+
+
+inputf = sys.argv[1]
+
+input_df = pd.read_csv(inputf, sep=",", header=0)
+
+nbr_rows = input_df.shape[0]
+gold_labels = input_df['hard_label']
+refusal_rate = (input_df['model_answer'] == 'Refused').sum()
+refusal_rate_nan = input_df['model_answer'].isna().sum()
+
+#print(input_df[input_df.isna().any(axis=1)])
+
+print("Number of entries: " + str(nbr_rows))
+print("Refused answers:" + str(refusal_rate))
+print("Missing answers:" + str(refusal_rate_nan))
+total_missed =  refusal_rate + refusal_rate_nan
+
+print("Refusal rate: " + str(total_missed/nbr_rows))
+
+input_df.replace({'model_answer': 'Refused'}, 0, inplace=True)
+input_df['model_answer'] = input_df['model_answer'].fillna(0)
+
+#predicted_labels = input_df['model_answer']
+predicted_labels = input_df['model_answer'].astype('int64')
+print(classification_report(gold_labels, predicted_labels, digits=4))
