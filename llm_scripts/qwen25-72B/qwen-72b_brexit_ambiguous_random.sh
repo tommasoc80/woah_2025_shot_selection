f4bf10f280b4b6dc5e9fdda4b636e4c5b7c2e460
tommasoc80 <t.caselli@gmail.com>
2025-03-04 20:43:57 +0100

    sbatch script qwen72B updates = zero shot output qwen 72B

--- llm_scripts/qwen25-72B/qwen-72b_brexit_ambiguous_random.sh
+++ llm_scripts/qwen25-72B/qwen-72b_brexit_ambiguous_random.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-#SBATCH --time=05:00:00
-#SBATCH --partition=gpu
-#SBATCH --gpus-per-node=a100:4
-#SBATCH --job-name=qwen72_ambiguous
-#SBATCH --mem=50G
+#SBATCH --partition=gpu_h100
+#SBATCH --time=00-08:00:00
+#SBATCH --gres=gpu:h100:4
+#SBATCH --mem=720G
 #SBATCH --output=brexit_ambiguous_random.out
 
-module load Python/3.11.3-GCCcore-12.3.0
 source ./bin/activate
 #python llama3_3b.py
 python qwen_7b_brexit_ambiguous_random.py --max-model-len 4096 --n-gpus 4
